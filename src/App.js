29af11c2522e0810aee0ff3ddb7b78989b5a4575
John <john1625b@gmail.com>
2022-02-13 06:57:44 -0500

    inital

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {data} from './data'
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedCol, setSelectedCol] = useState('')
+    const [sortDirection, setSortDirection] = useState(true);
+    const [playerNameInput, setPlayerNameInput] = useState('');
+    const columnHeaders = Object.keys(data[0])
+    const columnClicked = (col) => {
+        setSelectedCol(col)
+        setSortDirection(prevState => prevState * -1)
+    }
+    const sortByColumn = (data, col) => {
+        if (col) {
+            return data.sort((a,b) => {
+                if (a[col] < b[col]) {
+                    return -1 * sortDirection
+                } else if (a[col] > b[col]) {
+                    return 1 * sortDirection
+                }
+                return 0
+            })
+        }
+        return data
+    }
+    const filterPlayerName = (data, playerName) => {
+        if (playerNameInput) {
+            return data.filter(player => (player.Player).toLowerCase().includes(playerName.toLowerCase()))
+        }
+        return data
+    }
+    return (
+        <div className="App">
+            <input type="text" placeholder="Search player name" name="search"
+                   onChange={e => setPlayerNameInput(e.target.value)}/>
+            {
+                playerNameInput &&
+                <div>search input: {playerNameInput}</div>
+            }
+            <table>
+                <thead>
+                <tr>
+                    {
+                        columnHeaders.map(header => (<th key={header} onClick={() => columnClicked(header)}>{header}</th>))
+                    }
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    filterPlayerName(sortByColumn(data, selectedCol), playerNameInput).map(row => (
+                        <tr key={row.Player}>
+                            {Object.keys(row).map((column, i) => (<td key={`${row[column]}-${i}`}>{row[column]}</td>))}
+                        </tr>
+                    ))
+                }
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
